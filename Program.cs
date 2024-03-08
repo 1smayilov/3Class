dc177854527d40d2453ca6f613a41ca2ec0389cd
Elvin Ismayilov <0utsawg@gmail.com>
2024-03-08 23:33:10 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,163 @@
+﻿namespace _3Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("1-ci ededi daxil edin");
+            //int ədəd1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("2-ci ededi daxil edin");
+            //int ədəd2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Emeliyyati daxil edin (+, -, *, /, %)");
+            //string emeliyyat = Console.ReadLine();
+
+            //int result = 0;
+
+
+            // -------------------      If Else       ------------------------
+
+
+            //if (emeliyyat == "+")
+            //{
+            //    result = ədəd1 + ədəd2;
+            //}
+
+            //else if (emeliyyat == "-")
+            //{
+            //    result = ədəd1 - ədəd2;
+            //}
+
+            //else if (emeliyyat == "/")
+            //{
+            //    result = ədəd1 / ədəd2;
+            //}
+
+            //else if (emeliyyat == "*")
+            //{
+            //    result = ədəd1 * ədəd2;
+            //}
+
+            //else if (emeliyyat == "%")
+            //{
+            //    result = ədəd1 % ədəd2;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yanlış operator seçimi");
+            //}
+
+            //Console.WriteLine(result);
+
+
+            // -------------------      Switch Case       ------------------------
+
+
+
+
+            //Console.WriteLine("1-ci ededi daxil edin");
+            //int ədəd1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("2-ci ededi daxil edin");
+            //int ədəd2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Emeliyyati daxil edin (+, -, *, /, %)");
+            //string emeliyyat = Console.ReadLine();
+
+            //int result = 0;
+
+
+
+            //switch (emeliyyat)
+            //{
+            //    case "+":
+            //        result = ədəd1 + ədəd2;
+            //        break;
+
+            //    case "-":
+            //        result = ədəd1 - ədəd2;
+            //        break;
+
+            //    case "*":
+            //        result = ədəd1 * ədəd2;
+            //        break;
+
+            //    case "/":
+            //        result = ədəd1 / ədəd2;
+            //        break;
+
+            //    case "%":
+            //        result = ədəd1 % ədəd2;
+            //        break;
+
+            //}
+
+            //Console.WriteLine(result);
+
+
+
+
+            // Console.WriteLine("0 - 1000 arasinda bir eded daxil edin");
+            // int eded = int.Parse(Console.ReadLine());
+
+            //string result = eded < 0 ? "Eded menfi deyer alib" :
+            //                eded < 10 ? "Eded 1 reqemlidir" :
+            //                eded < 100 ? "Eded 2 reqemlidir" :
+            //                eded < 1000 ? "Eded 3 reqemlidir" :
+            //                "Eded 0 - 1000 araliginda deyil";
+
+            //Console.WriteLine(result);
+
+
+            //Console.WriteLine("0 - 1000 arasında bir sayı girin:");
+            //int eded = int.Parse(Console.ReadLine());
+
+            //switch (eded.ToString().Length)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Eded 1 reqemlidir");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Eded 2 reqemlidir");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Eded 3 reqemlidir");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Eded 4 reqemlidir");
+            //        break;
+            //}
+
+
+            //int result = 1;
+            //for (int i = 1; i <= 10; i += 2)
+            //{
+            //    result *= i;
+            //}
+            //    Console.WriteLine(result);
+
+
+            
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+    }
+    
+
